efa1d4ddaae54a5b2f88534dccc42dbaf4d035f4
leiner117 <leineralvarador117@gmail.com>
2025-10-11 07:39:20 -0600

    refactor: temporarily disable ProjectsSection in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import useHomeViewModel from "./components/home/useHomeViewModel";
 import HeroSection from "./components/home/HeroSection";
 import ExperienceSection from "./components/home/ExperienceSection";
-import ProjectsSection from "./components/home/ProjectsSection";
+// import ProjectsSection from "./components/home/ProjectsSection"; // temporarily disabled
 import SkillsSection from "./components/home/SkillsSection";
 import ContactSection from "./components/home/ContactSection";
 
@@ -12,7 +12,7 @@ const Home = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
   <HeroSection />
   <ExperienceSection />
-  <ProjectsSection />
+  {/* <ProjectsSection /> */}
   <SkillsSection />
   <ContactSection />
     </div>
