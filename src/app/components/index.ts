7153d5534c36a21f5e6bfc583d74e7ba5068a063
leiner117 <leineralvarador117@gmail.com>
2025-10-16 19:55:46 -0600

    feat: add missing exports for FlameBackground, MagneticButton, and SocialLinks components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,6 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as List } from "./list/List";
 export { GoogleAnalytics } from "./googleAnalytics/GoogleAnalytics";
+export { FlameBackground } from "./flame-background/Flame-backgound";
+export { MagneticButton } from "./magnetic-button/MagneticButton";
+export { SocialLinks } from "./social-links/socialLinks";
